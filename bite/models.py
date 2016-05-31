361209226247c4c4e819c5ecd56310cc54969d4f
Avihai Yosef <avihai123@hotmail.com>
2016-05-31 16:54:05 +0300

    Added customer model

--- bite/models.py
+++ bite/models.py
@@ -1,7 +1,13 @@
+from django.conf import settings
+from django.core.validators import RegexValidator
 from django.db import models
 
 
 # Create your models here.
+# TODO move this validator to somewhere else!
+name_validator = RegexValidator(r'^[a-zA-Z]+$')
+
+# TODO add imagaField for restaurant image, resize it.
 class Restaurant(models.Model):
     # Relations
     # Attributes - Mandatory
@@ -66,3 +72,77 @@ class Dish(models.Model):
 
     def __str__(self):
         return "{}-{}-{}".format(self.name, self.price, self.desc)
+
+class Company(models.Model):
+    # Relations
+    restaurant = models.ManyToManyField(
+        Restaurant,
+        related_name='companies',
+        verbose_name='restaurant',
+        null=True,
+        blank=True,
+    )
+    # Attributes - Mandatory
+    name = models.CharField(
+        max_length=30,
+        validators=[],
+    )
+    address = models.CharField(
+        max_length=100,
+        verbose_name='address',
+        help_text='Enter valid address(City, Street, Number)',
+        # TODO regex validator for all languages.
+        validators=[],
+    )
+
+    # TODO regex validator for all languages.
+    phone = models.CharField(
+        max_length=10,
+        verbose_name='phone',
+        validators=[],
+        help_text='Enter valid phone number',
+    )
+
+    email = models.EmailField(verbose_name='email')
+
+    active = models.BooleanField(default=True)
+    owner = models.CharField(max_length=30)
+    # Attributes - Optional
+    description = models.TextField(blank=True, null=True)
+
+    # Object Manager
+    # Custom Properties Methods
+    # Meta and String
+    class Meta:
+        default_related_name = 'companies'
+        verbose_name = 'company'
+        verbose_name_plural = 'companies'
+
+class Customer(models.Model):
+    # Relations
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='customers'
+    )
+    company = models.ForeignKey(
+        Company,
+        on_delete=models.CASCADE,
+        related_name='customers'
+    )
+    # Attributes - Mandatory
+    worker_id = models.PositiveIntegerField(primary_key=True)
+    first_name = models.CharField(
+        max_length=30,
+        validators=[name_validator],
+    )
+
+    last_name = models.CharField(
+        max_length=30,
+        validators=[name_validator],
+    )
+
+    email = models.EmailField(verbose_name='email')
+    # Attributes - Optional
+    # Object Manager
+    # Custom Properties Methods
+    # Meta and String
--- bite/views.py
+++ bite/views.py
@@ -14,3 +14,6 @@ class IndexView(ListView):
 
 class RestaurantListView(ListView):
     model = Restaurant
+
+class RestaurantDetailView(DetailView):
+    model = Restaurant
