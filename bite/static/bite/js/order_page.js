75196b8e060f7f82a51365221e2499e31428ab45
Alonski <alonzorz@gmail.com>
2016-06-02 21:23:57 +0300

    Fixed Static Pages Bug. Had to restart server

--- bite/static/bite/js/order_page.js
+++ bite/static/bite/js/order_page.js
@@ -1,22 +1,20 @@
-// /**
-//  * Created by Alonski on 6/2/2016.
-//  */
-//
-// alert("Test");
-//
-// $(function () {
-//     console.log("start");
-//
-//     $('#dishes').on('click', 'li', function () {
-//         var el = $(this);
-//         console.log(el);
-//         var new_el = $('<li>').text(el.text());
-//         new_el.append($('<input type="hidden" name="item">').val(el.data('id')));
-//         $('#frm ul').append(new_el);
-//     });
-//
-//     console.log("end");
-//
-//     // requst.POST['item'].getlist()
-//
-// });
+/**
+ * Created by Alonski on 6/2/2016.
+ */
+
+$(function () {
+    console.log("start");
+
+    $('#dishes').on('click', 'li', function () {
+        var el = $(this);
+        console.log(el);
+        var new_el = $('<li>').text(el.text());
+        new_el.append($('<input type="hidden" name="item">').val(el.data('id')));
+        $('#frm ul').append(new_el);
+    });
+
+    console.log("end");
+
+    // requst.POST['item'].getlist()
+
+});
